8e1d5ce4bc59792bdc7db955b2a943f8a9f5f626
Michael Scott <michael.scott@transferwise.com>
2021-06-18 10:09:12 +0300

    Registration Client Refactoring (PAYINP-962)
    Refactor the registration client so that the OAuth access token to use for the update client registration API call to the ASPSP, is passed in by the caller rather than being fetched by the registration client internally.
    
    Further refactor the registration client implementation so that when the API call fails because the ASPSP returned a non-successful response, we return a response wrapper that signals the call failed, rather than throwing an exception. This failure response wrapper contains the details of the response, the failure, and the parsed structured error response.
    
    This follows the same refactoring done for the payment client, to give callers easy access to the parsed structured error response on API call failure.

--- src/main/java/com/transferwise/openbanking/client/api/registration/RegistrationClient.java
+++ src/main/java/com/transferwise/openbanking/client/api/registration/RegistrationClient.java
@@ -1,7 +1,9 @@
 package com.transferwise.openbanking.client.api.registration;
 
+import com.transferwise.openbanking.client.api.common.ApiResponse;
 import com.transferwise.openbanking.client.api.registration.domain.ClientRegistrationResponse;
 import com.transferwise.openbanking.client.api.registration.domain.ClientRegistrationRequest;
+import com.transferwise.openbanking.client.api.registration.domain.ErrorResponse;
 import com.transferwise.openbanking.client.configuration.AspspDetails;
 import com.transferwise.openbanking.client.configuration.SoftwareStatementDetails;
 
@@ -15,27 +17,30 @@ public interface RegistrationClient {
      *
      * @param clientRegistrationRequest The details (JWT claims) of the registration request
      * @param aspspDetails              The details of the ASPSP to send the request to
-     * @return The response body from the ASPSP, containing the details of the registration
-     * @throws com.transferwise.openbanking.client.error.ClientException if there was a problem building the request(s)
-     *                                                                   to the ASPSP or the HTTP call to the ASPSP
-     *                                                                   failed
+     * @return The response, from the ASPSP, to the client registration request
+     * @throws com.transferwise.openbanking.client.error.ClientException if there was a problem building the request to
+     *                                                                   to the ASPSP, or there was a problem parsing
+     *                                                                   the response when the API call succeeded
      */
-    ClientRegistrationResponse registerClient(ClientRegistrationRequest clientRegistrationRequest,
-                                              AspspDetails aspspDetails);
+    ApiResponse<ClientRegistrationResponse, ErrorResponse> registerClient(ClientRegistrationRequest clientRegistrationRequest,
+                                                                          AspspDetails aspspDetails);
 
     /**
      * Update an existing TPP client registration with an ASPSP.
      *
      * @param clientRegistrationRequest The details (JWT claims) of the new registration request, this MUST contain
      *                                  both the claims to change, and those which are unchanged
+     * @param clientCredentialsToken    The client credentials access token, obtained from the ASPSPs OAuth API, to
+     *                                  use for the authorization for this ASPSP API call
      * @param aspspDetails              The details of the ASPSP to send the request to
      * @param softwareStatementDetails  The details of the software statement that the ASPSP registration currently uses
-     * @return The response body from the ASPSP, containing the details of the updated registration
-     * @throws com.transferwise.openbanking.client.error.ClientException if there was a problem building the request(s)
-     *                                                                   to the ASPSP or the HTTP call to the ASPSP
-     *                                                                   failed
+     * @return The response, from the ASPSP, to the update client registration request
+     * @throws com.transferwise.openbanking.client.error.ClientException if there was a problem building the request to
+     *                                                                   to the ASPSP, or there was a problem parsing
+     *                                                                   the response when the API call succeeded
      */
-    ClientRegistrationResponse updateRegistration(ClientRegistrationRequest clientRegistrationRequest,
-                                                  AspspDetails aspspDetails,
-                                                  SoftwareStatementDetails softwareStatementDetails);
+    ApiResponse<ClientRegistrationResponse, ErrorResponse> updateRegistration(ClientRegistrationRequest clientRegistrationRequest,
+                                                                              String clientCredentialsToken,
+                                                                              AspspDetails aspspDetails,
+                                                                              SoftwareStatementDetails softwareStatementDetails);
 }
