4fe6c66eabc490a6045a085c6c68db7ca6d581f0
Vivek Kumar <vivek.kumar@transferwise.com>
2023-02-06 18:04:44 +0000

    Fixing Javadoc
    Signed-off-by: Vivek Kumar <vivek.kumar@transferwise.com>

--- src/main/java/com/transferwise/openbanking/client/api/event/EventClient.java
+++ src/main/java/com/transferwise/openbanking/client/api/event/EventClient.java
@@ -1,24 +1,27 @@
 package com.transferwise.openbanking.client.api.event;
 
 import com.transferwise.openbanking.client.api.payment.v3.model.event.OBEventSubscription1;
+import com.transferwise.openbanking.client.api.payment.v3.model.event.OBEventSubscriptionResponse1;
 import com.transferwise.openbanking.client.api.payment.v3.model.event.OBEventSubscriptionsResponse1;
 import com.transferwise.openbanking.client.configuration.AspspDetails;
 import com.transferwise.openbanking.client.configuration.SoftwareStatementDetails;
-import com.transferwise.openbanking.client.api.payment.v3.model.event.OBEventSubscriptionResponse1;
 
 /**
- *  Interface specifying operations supported for event API open banking in 3.1.10
+ * Interface specifying operations supported for open banking event API in 3.1.10.
+ *
+ * @since 12.0.0
  */
 public interface EventClient {
 
     /**
-     * Create an Event Subscription :
-     * Asking a ASPSP to notify callback whenever there are updates on the given event.
+     * Subscribe to an event.
      *
-     * @param eventSubscriptionRequest  request for event contains {callback URL, event type, version}
-     * @param aspspDetails -> detail of ASPSP which has the event to subscribe.
-     * @param softwareStatementDetails ->  The details of the software statement that the ASPSP registration uses
-     * @return OBEventSubscriptionResponse1 -> Event {subscription id, callback URL, version, event types}
+     * @param eventSubscriptionRequest  The request for event subscription.
+     * @param aspspDetails  The details of the ASPSP.
+     * @param softwareStatementDetails  The details of the software statement for ASPSP
+     *                                 registration.
+     * @return OBEventSubscriptionResponse1 The details of event subscribed.
+     * @throws EventApiCallException    If subscribe API call to ASPSP fails
      */
     OBEventSubscriptionResponse1 subscribeToAnEvent(
         OBEventSubscription1 eventSubscriptionRequest,
@@ -26,18 +29,21 @@ public interface EventClient {
         SoftwareStatementDetails softwareStatementDetails);
 
     /**
-     * Get all events from the given ASPSP.
+     * Get all events.
      *
-     * @param aspspDetails details of ASPSP which has the subscribed event resources.
-     * @return OBEventSubscriptionsResponse1 set of events subscribed to the given ASPSP.
+     * @param aspspDetails  The details of the ASPSP.
+     * @return OBEventSubscriptionsResponse1    List of subscribed events.
+     * @throws EventApiCallException    If get all events API call to ASPSP fails
      */
     public OBEventSubscriptionsResponse1 getEventResources( AspspDetails aspspDetails);
 
     /**
-     * Ask ASPSP to change a subscribed event. .
+     * Change a subscribed event.
      *
-     * @param changedResponse new response with new entries like callbackURL, eventTypes, version.
-     * @param aspspDetails details of ASPSP which has the subscribed event resources.
+     * @param changedResponse   The details for changing a subscribed event.
+     * @param aspspDetails  The details of the ASPSP.
+     * @return OBEventSubscriptionResponse1 The details of the changed event resource.
+     * @throws EventApiCallException    If change Event API call to ASPSP fails
      */
     public OBEventSubscriptionResponse1 changeAnEventResource(
         OBEventSubscriptionResponse1 changedResponse,
@@ -45,11 +51,11 @@ public interface EventClient {
         SoftwareStatementDetails softwareStatementDetails);
 
     /**
-     * Ask ASPSP to delete the given subscribed Event resource.
+     * Delete an event.
      *
-     * @param eventSubscriptionId Unique identification as assigned by the ASPSP,
-     *                           to uniquely identify the callback URL resource.
-     * @param aspspDetails details of ASPSP which has the subscribed event resources.
+     * @param eventSubscriptionId   The Unique identification for an event resource.
+     * @param aspspDetails  The details of the ASPSP.
+     * @throws EventApiCallException    If delete event API call to ASPSP fails
      */
     public void deleteAnEventResource(
         String eventSubscriptionId, AspspDetails aspspDetails);
